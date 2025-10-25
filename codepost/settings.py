f30f8788002ce2a30046a78819ab9f93fd8256e0
Mohamad Khawam <mk1800@rutgers.edu>
2025-10-25 01:19:14 -0400

    added frontend machines to csrf

--- codepost/settings.py
+++ codepost/settings.py
@@ -173,7 +173,9 @@ CSRF_TRUSTED_ORIGINS = (
     "http://localhost",
     "https://localhost",
     "http://*.cs.rutgers.edu",
-    "https://*.cs.rutgers.edu",
+    "https://dev-codepost-1.cs.rutgers.edu",
+    "https://codepost.cs.rutgers.edu",
+    
 )
 
 checkCSRFTrustedOrigins(CLIENT_URL, CSRF_TRUSTED_ORIGINS)
